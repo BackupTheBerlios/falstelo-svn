76777917a1dc2f8363cbc338f55992140d0383b4
bouil <bouil@92e45aa6-3ed6-0310-a01c-b117b8fb0086>
2004-03-22 23:13:32 +0000

    Ajout du fichier de functions remplacant une partie du fichiers variables.php

---
+++ lib/functions.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * (c) Nicolas Bouillon
+ * Ce fichier est distribué selon la licence GNU GPL.
+ */
+
+error_reporting(E_ERROR |  E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR );
+
+setlocale(LC_MONETARY, 'fr_FR');
+
+// pour php <4.3.0
+if (!function_exists("file_get_contents")) 
+{
+  function file_get_contents($filename, $use_include_path = 0) 
+    {
+      $data = ""; // just to be safe. Dunno, if this is really needed
+      $file = @fopen($filename, "rb", $use_include_path);
+      if ($file) 
+	{
+	  while (!feof($file)) $data .= fread($file, 1024);
+	  fclose($file);
+	}
+      return $data;
+    }
+}
+?>
